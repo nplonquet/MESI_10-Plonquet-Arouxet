c4b3bbda97c1987386c852cc36140655dab3db90
nicolas PLONQUET <nicolas.plonquet@gmail.com>
2017-11-26 18:33:41 +0100

    ffichage page de profil plus topic

--- profil.js
+++ profil.js
@@ -7,14 +7,17 @@ var estConnecte = false;
 //création d'un prototype utilisateurs
 var utilisateur = {
     //initialiser l'utilisateurs
-    initUser: function (id, date, genre, mail, password, pseudo, preferences) {
+    initUser: function (id, date, genre, mail, password, username, preferencesJ, preferencesF, preferencesS, image) {
         this.id = id;
         this.mail = mail;
         this.password = password;
-        this.pseudo = pseudo;
-        this.preferences = preferences;
+        this.username = username;
+        this.preferencesJ = preferencesJ;
+        this.preferencesF = preferencesF;
+        this.preferencesS = preferencesS;
         this.date = date;
         this.genre = genre;
+        this.image = image;
     },
     //ajouter cet utilisateur dans le tableau
     ajouter: function () {
@@ -24,15 +27,18 @@ var utilisateur = {
 
 //Charger tous les utilisateurs présents dans le fichier json pour les ajouter dans le tableau
 $(document).ready(function ($) {
+    $("body").hover(function(){
+        console.log("hello")
+        user_info(utilisateurs, 1)
+    })
     $.get(
         'utilisateur.json',
         function (data) {
             var i = 0;
             while (i < data.user.length) {
                 var util = Object.create(utilisateur);
-                util.initUser(data.user[i].id, data.user[i].date, data.user[i].genre , data.user[i].email, data.user[i].password, data.user[i].username, data[i].user[i].preferences);
+                util.initUser(data.user[i].id, data.user[i].date, data.user[i].genre , data.user[i].email, data.user[i].password, data.user[i].username, data.user[i].preferencesJ, data.user[i].preferencesF, data.user[i].preferencesS, data.user[i].image);
                 util.ajouter();
-                console.log(data.user[i]);
                 i++;
             }
         },
@@ -50,15 +56,25 @@ function user_info(utilisateurs, id) {
     console.log(utilisateurs)
     current_user = []
     for (var i = 0; i < utilisateurs.length; i++){
-        if (id == utilisateur[i].id){
-            current_user = utilisateur[i]
+        if (id == utilisateurs[i].id){
+            current_user = utilisateurs[i]
         }
     }
+    console.log(current_user)
 
     $('#nom').html(current_user.username)
-    $('#genre').html()
-    $('#date').html(current_user.date)
-    //$('#pref1j').html(current_user.preferences[0])
+    $('#genre').html(current_user.genre)
+    $('#date').html("Inscrit le: " + current_user.date)
+    $('#ps1').html(current_user.preferencesS[0])
+    $('#ps2').html(current_user.preferencesS[1])
+    $('#ps3').html(current_user.preferencesS[2])
+    $('#pf1').html(current_user.preferencesF[0])
+    $('#pf2').html(current_user.preferencesF[1])
+    $('#pf3').html(current_user.preferencesF[2])
+    $('#pj1').html(current_user.preferencesJ[0])
+    $('#pj2').html(current_user.preferencesJ[1])
+    $('#pj3').html(current_user.preferencesJ[2])
+    $('#image').html('<img src = "' + current_user.image + '"/>')
     //$('#pref2j').html(urrent_user.preferences[1])
 }
 // $(document).ready(function ($) {
